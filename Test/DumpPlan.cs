32dc6a4f5fb87b3722d44716899b259a987fd3c8
xtovo <valeriy.onuchin@yandex.ru>
2021-03-29 07:40:28 +0300

    - add

---
+++ Test/DumpPlan.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using TM;
+using TM.Properties;
+
+namespace Dump
+{
+	public class Plan
+	{
+		public Dictionary<int, PlanSpot> Load()
+      {  
+         var plan = TMClient.LoadPlanData("test_plan.txt");
+
+         Console.WriteLine("\nПродолжить ...");
+         Console.WriteLine("__________________________________________________________________________");
+         Console.ReadKey();
+         
+         return plan;
+		}
+	}
+}
