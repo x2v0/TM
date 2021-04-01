9f5635ad3cee2891a7264a1b3d2bcfef532f1e5c
xtovo <valeriy.onuchin@yandex.ru>
2021-04-01 11:03:22 +0300

    - fix pbuild.ps1 double-clicking

--- pbuild.ps1
+++ pbuild.ps1
@@ -1,4 +1,10 @@
-# Author: Valeriy Onuchin   31.03.2021
+# Author: Valeriy Onuchin 31.03.2021
+
+# Set the current directory by "Right-Mouse Click" -> "Context menu" -> "Open with" -> "Windows PowerShell"
+#  ... or by Double-Clicking
+$currentScriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
+[System.IO.Directory]::SetCurrentDirectory($currentScriptDirectory)
+Set-Location $currentScriptDirectory
 
 dotnet build --configuration Release ./TM.sln
 
