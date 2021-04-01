896fe1a7adfceb7162047d50a99d27436ed6190b
xtovo <valeriy.onuchin@yandex.ru>
2021-04-01 15:23:19 +0300

    - ... fix cleaning

--- pclean.bat
+++ pclean.bat
@@ -3,16 +3,15 @@
 ::
 :: Clean output folders
 
+
 for /D /R %%i in (bin*) do (rd /s /q "%%i")
-for /D /R %%i in (obj*) do (rd /s /q "%%i")
+for /D /R %%i in (obj) do (rd /s /q "%%i")
 for /D /R %%i in (redist*) do (rd /s /q "%%i")
 for /D /R %%i in (debug*) do (rd /s /q "%%i")
 for /D /R %%i in (release*) do (rd /s /q "%%i")
 for /D /R %%i in (precompiledweb*) do (rd /s /q "%%i")
 for /D /R %%i in (x64*) do (rd /s /q "%%i")
 ::for /D /R %%i in (_ReSharper*) do (rd /s /q "%%i")
-
-
 
 :: Clean generated files
 
