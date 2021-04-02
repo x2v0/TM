cb1a7427ea74f2504d8656a76e3dfd57d521f05a
xtovo <valeriy.onuchin@yandex.ru>
2021-04-02 12:24:44 +0300

    - renaming

---
+++ build.bat
@@ -0,0 +1,8 @@
+::  Author: Valeriy Onuchin   2.04.2021
+
+@ECHO OFF
+
+PUSHD %~dp0
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command ".\build.ps1 %*; exit $LastExitCode;"
+POPD
+
--- pbuild.bat
+++ pbuild.bat
@@ -1,7 +1,3 @@
-:: $Id: pbuild.bat 7729 2020-01-24 09:42:56Z onuchin $
-:: Author: Valeriy Onuchin   19.04.2014
-::
-
 call defines.bat
 %msbuild%  .\TM.vs10.sln /t:Build /p:Configuration=release
 
